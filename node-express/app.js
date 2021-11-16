c7a3867dc488e54d23097a51b955fdd820fce6aa
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-16 16:46:46 +0900

    reqest 에 대해서

--- node-express/app.js
+++ node-express/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
 const app = express();
 
-app.get('/', (req, res, next)=>{
-    console.log('get');
+app.get('/sky/:id', (req, res, next)=>{
+    console.log(req.path);
+    console.log(req.headers);
+    console.log(req.params.id);
+    console.log(req.query);
+    console.log(req.query.keyword);
     res.send('hi!');
 });
 
