11df8fae70ec59bbe0dfdc434c43e0d97c2f28a1
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-16 16:56:39 +0900

    response에 대해서

--- node-express/app.js
+++ node-express/app.js
@@ -7,7 +7,12 @@ app.get('/sky/:id', (req, res, next)=>{
     console.log(req.params.id);
     console.log(req.query);
     console.log(req.query.keyword);
-    res.send('hi!');
+
+    res.setHeader('key', 'value');
+    res.status(201).send('created');
+    // res.sendStatus(400);
+    // res.json({name:'Liam'});
+    //res.send('hi!');
 });
 
 app.listen(8080);
